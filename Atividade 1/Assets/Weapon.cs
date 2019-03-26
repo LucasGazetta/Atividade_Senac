7dfe70dbb13a105b1cb9bc09a6b217ea64a93551
LucasGazetta <lucasagazetta@gmail.com>
2019-03-26 11:15:49 -0300

    spawn da bala consertado e rigid body implementado

--- Atividade 1/Assets/Weapon.cs
+++ Atividade 1/Assets/Weapon.cs
@@ -7,11 +7,6 @@ public class Weapon : MonoBehaviour
 
     public Transform firePoint;
     public GameObject bulletPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
